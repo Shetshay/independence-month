46942196ceef58ebb6d890772bf219bc06347bbb
Justin Alejo <justinalejo@gmail.com>
2023-09-27 10:10:23 -0700

    adding justin.h and justin.cpp

---
+++ jalejo.cpp
@@ -0,0 +1,17 @@
+
+#include <GL/glx.h>
+//#include "land.h"
+//#include "lander.cpp"
+
+void test_function(){
+
+    glPushMatrix();
+    glBegin(GL_QUADS);
+    glColor3ub(250, 200, 90); glVertex2i(0,0);
+    glColor3ub(100,  80, 200); glVertex2i(0, 500);
+    glColor3ub(100, 80, 200); glVertex2i(0, 500);
+    glColor3ub(250, 200, 90); glVertex2i(0,0);
+    glPopMatrix();
+    glEnd();
+
+}
---
+++ jalejo.h
@@ -0,0 +1,13 @@
+/*#ifndef JUSTIN
+#define JUSTIN*/
+
+using namespace std;
+
+extern void test_function();
+
+/*void game_over(){
+
+    cout << "GAME OVER: PRESS R TO TRY AGAIN" << endl;
+}*/
+
+//#endif
